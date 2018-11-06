38048a8080af5e22a5c7915830a71329dfc658f0
Rhaegal <dapsjj@qq.com>
2018-11-06 16:12:58 +0800

    Create CabochaVO.java

---
+++ CabochaVO.java
@@ -0,0 +1,35 @@
+package sjjTEST;
+
+public class CabochaVO {
+	private String keyWord;
+	private String originalForm;
+	private String property;
+	private String detail;
+	
+	
+	public String getKeyWord() {
+		return keyWord;
+	}
+	public void setKeyWord(String keyWord) {
+		this.keyWord = keyWord;
+	}
+	public String getOriginalForm() {
+		return originalForm;
+	}
+	public void setOriginalForm(String originalForm) {
+		this.originalForm = originalForm;
+	}
+	public String getProperty() {
+		return property;
+	}
+	public void setProperty(String property) {
+		this.property = property;
+	}
+	public String getDetail() {
+		return detail;
+	}
+	public void setDetail(String detail) {
+		this.detail = detail;
+	}
+	
+}
